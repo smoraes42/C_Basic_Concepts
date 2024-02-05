edd28c71a8e8fe3898b8ef87caef063e5054245c
smoraes- <153234158+smoraes42@users.noreply.github.com>
2024-02-05 02:24:30 +0100

    order

--- typedef_structures_and_lists/structures_and_functions3.c
+++ typedef_structures_and_lists/structures_and_functions3.c
@@ -6,17 +6,18 @@
 /*   By: smoraes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 19:35:00 by smoraes-          #+#    #+#             */
-/*   Updated: 2024/02/03 19:54:41 by smoraes-         ###   ########.fr       */
+/*   Updated: 2024/02/04 22:48:56 by smoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct point
 {
 	int x;
 	int y;
-}
+};
 
 // Returning a structure variable from the function
 struct point edit(struct point p)
@@ -25,7 +26,7 @@ struct point edit(struct point p)
 	p.y = p.y + 5;
 	return p;
 }
-void print(struct point p)
+void print_value(struct point p)
 {
 	printf("%d %d\n", p.x, p.y);
 }
@@ -33,34 +34,34 @@ void print(struct point p)
 // Returning a pointer to a structure from the function
 struct point *fun(int a, int b)
 {
-	struct point *ptr = (struct point *)malloc(sizeof(truct point));
+	struct point *ptr = (struct point *)malloc(sizeof(struct point));
 	ptr->x = a;
 	ptr->y = b + 5;
 	return ptr;
 }
-void print(structure point *ptr)
+void print_ref(struct point *ptr)
 {
 	printf("%d %d\n", ptr->x, ptr->y);
 }
 
 int main(void)
 {
-	struct point ptr1 = {23, 45};
-	struct point ptr2 = {56, 90};
+	struct point p1 = {23, 45};
+	struct point p2 = {56, 90};
 
 	// Returning a structure variable from the function
 	p1 = edit(p1);
 	p2 = edit(p2);
-	print(p1);
-	print(p2);
+	print_value(p1);
+	print_value(p2);
 
 	// Retuning a pointer from the function
 	struct point *ptr1;
 	struct point *ptr2;
 	ptr1 = fun(2,3);
 	ptr2 = fun(6,9);
-	print(ptr1);
-	print(ptr2);
+	print_ref(ptr1);
+	print_ref(ptr2);
 	free(ptr1);
 	free(ptr2);
 
